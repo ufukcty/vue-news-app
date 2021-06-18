517039980de223739ef40ae48d975fa214d607cf
ufukcty <ufukcgtyalks@gmail.com>
2021-06-18 11:36:30 +0300

    first release

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="flex justify-center py-6 bg-red-900 place-items-center">
+    <img src="../assets/logo.png" alt="Vue logo" width="180"/>
+    <span class="ml-4 text-lg font-bold text-red-100 md:text-xl">
+      Vue News | NYTimes Edition
+    </span>
+  </header>
+</template>
